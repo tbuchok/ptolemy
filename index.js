524cce73df7e64cdfe464565c8b5f0daf482899c
Tom Buchok <tom@buchok.com>
2013-07-22 10:54:44 -0400

    uses object.create everywhere ...

--- index.js
+++ index.js
@@ -1,124 +1,80 @@
-var util = require('util')
+var Level = require('level')
+  , SubLevel = require('level-sublevel')
+  , db = SubLevel(Level('./db-ptolemy'))
+  , util = require('util')
   , events = require('events')
 ;
 
-var _adapter = {
-    all : function(cb) { cb('#all not implemented') }
-  , find : function(k, cb) { cb('#find not implemented') }
-  , save : function(k, v, cb) { cb('#save not implemented') }
+var Ptolemy = {
+  dbs : {}
 };
 
-var guid = function () {
+var guid = function() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
       return v.toString(16);
   });
 };
 
-function Ptolemy(data) {
-  data = data || {};
+var create = function create(name) {
+  
+  if (Ptolemy.dbs[name] !== undefined)
+    throw new Error('DB already exists!');
+  
+  Ptolemy.dbs[name] = name;
 
-  this._id = data._id || guid();
-  this._key = this.key();
-  this._attrs = {};
-  this._types = {
-      'string'  : String
-    , 'num'     : Number
-    , 'array'   : Array
-    , 'object'  : Object
-    , 'date'    : Date
-  };
+  var _db = db.sublevel(name);
 
-  // Model-specific properties below ...
-}
-util.inherits(Ptolemy, events.EventEmitter);
-
-Ptolemy.adapter = function(adapter) {
-  _adapter = adapter;
-};
-
-Ptolemy.all = function(cb) {
-  _adapter.all(cb);
-};
-
-Ptolemy.prototype.key = function() {
-  var result = '\xFF' + this.constructor.name + '\x00';
-  return result;
-};
-
-Ptolemy.prototype.createAttr = function(attr, Type) {
-  this._attrs[attr] = new Type().constructor.name;
-};
-
-Ptolemy.find = function(id, cb) {
-  var key = Ptolemy.prototype.key() + id;
-  _adapter.find(key, function(err, data) {
-    if (err) return cb(err);
-    cb(null, new Ptolemy(data));
-  });
-};
-
-Ptolemy.prototype.toJSON = function () {
-  return JSON.stringify(this.toObject());
-}
-
-Ptolemy.prototype.toObject = function() {
-  var object = {};
-  var errors = [];
-  var looper = function(value) {
-    if (this._attrs[value] === this[value].constructor.name)
-      object[value] = this[value]
-    else
-      errors.push(value + ' type incorrect: expected' + this._attrs[value] + ' received ' + this[value].constructor.name);
-  };
-  Object.keys(this._attrs).forEach(looper.bind(this));
-  object['_id'] = this._id;
-  object['_key'] = this._key;
-  return (errors.length) ? errors : object;
-}
-
-Ptolemy._hooks = { beforeSave: [], afterSave: [] };
-
-Ptolemy.beforeSave = function(fn) {
-  if (fn.constructor.name === 'Function')
-    Ptolemy._hooks['beforeSave'].push(fn);
-};
-
-Ptolemy.afterSave = function(fn) {
-  if (fn.constructor.name === 'Function')
-    Ptolemy._hooks['afterSave'].push(fn);
-};
-
-Ptolemy.prototype.save = function(cb) {
-  var self = this;
-  Ptolemy._hooks['beforeSave'].forEach(function(fn){ fn.call(self); });
-
-  var key = this.key(this.id)
-    , value = this.toObject()
-  ;
-
-  if (Array.isArray(value)) // Errors occured!
-    return (cb) ? cb(value) : this.emit('error', value);
-
-  var handleSave = function(err) {
-    if (err) {
-      (cb) ? cb(err) : this.emit('error', err);
-    } else {
-     Ptolemy._hooks['afterSave'].forEach(function(fn){ fn.call(self) }); 
-     if (cb) cb(err);
+  return {
+      _db: _db
+    , schema: {}
+    , create: function(options) {
+      options = options || {};
+      var instance = Object.create(this);
+      instance._id = options._id || guid();
+      Object.keys(options).forEach(function(key) {
+        if (instance.schema.hasOwnProperty(key)) {
+          if (typeof options[key] === instance.schema[key])
+            instance[key] = options[key]
+          else
+            throw new Error('Expecting type ' + instance.schema[key] + ' but got type ' + typeof key);
+        }
+      });
+      return instance;
     }
-  };
-  var key = this._key + this._id
-  _adapter.save(key, value, handleSave.bind(this));
-};
+    , save: function(cb) {
+      _db.put(this._id, JSON.stringify(this), function(err) {
+        if (cb) return cb(err);
+        else if (err) emit('error', err);
+      });
+    }
+    , find: function(query, cb) {
+      var self = this;
+      _db.get(query, function(err, data) {
+        if (err) return cb(err);
+        try {
+          var options = JSON.parse(data);
+          var instance = self.create(options);
+          cb(null, instance);
+        } catch(e) { cb(e); };
+      });
+    }
+  }
+}
 
-Ptolemy.prototype.error = function(err) {
-  this.emit('error', err);
-};
- 
-// must be able to run queries
-// must be able to have relationships
-// must be able to run batch jobs
-// must be able to stream out data
+var schema = function schema(attrs) {
+  attrs = attrs || {};
+  var result = {};
+  Object.keys(attrs).forEach(function(key){
+    var type = new attrs[key]().constructor.name;
+    result[key] = type.toLowerCase();
+  });
+  return result;
+}
 
-module.exports = Ptolemy;
+Ptolemy.create = create;
+Ptolemy.schema = schema;
+
+util.inherits(Ptolemy, events.EventEmitter)
+
+var ptolemy = module.exports = exports = Object.create(Ptolemy);
--- test/index.js
+++ test/index.js
@@ -1,96 +1,92 @@
 var test = require('tap').test
   , Ptolemy = require('..')
-  , Adapter = require('../Adapter')
-  , util = require('util')
 ;
 
 test('basic Ptolemy operations', function(t) {
-  var adapter = new Adapter();
-  Ptolemy.adapter(adapter);
-  var p = new Ptolemy();
-  t.ok(p, 'Creates Ptolemy instances');
-  t.ok(p.save,'Has a #save method');
-  t.ok(/^\xFFPtolemy\x00/.test(p._key), 'It creates a key based on the constructor');
-  p.save(function(err) {
-    t.ok(err === null, 'It successfully saves');
-    Ptolemy.find(p._id, function(err, model) {
-      t.ok(err === null, 'It successfully finds');
-      t.equals(p._id, model._id, 'It finds the correct record');
+  var Explorer = Ptolemy.create('Explorer');
+  Explorer.schema = Ptolemy.schema({ name: String, voyages: Number });
+  var explorer = Explorer.create({ name: 'Christopher Columbus', voyages: 4 });
+  t.ok(explorer, 'It creates instances');
+  t.ok(explorer.save, 'It has a save method');
+  explorer.save(function(err) {
+    if (err) throw new Error(err);
+    t.notOk(err, 'It does not find an error on save');
+    Explorer.find(explorer._id, function(err, result) {
+      t.equal(explorer._id, result._id, 'It finds the correct explorer!');
       t.end();
     });
   });
 });
 
 test('psuedo-sync saving', function(t) {
-  var p = new Ptolemy();
-  p.save();
+  var P = Ptolemy.create('P');
+  P.schema = Ptolemy.schema({});
+  var p = P.create({});
   t.end();
 });
 
-test('inheritance', function(t) {
-  function Foo(){
-    Ptolemy.call(this);
-  }
-  util.inherits(Foo, Ptolemy);
-  var foo = new Foo();
-  t.ok(/^\xFFFoo\x00/.test(foo._key), 'It creates a key based on the constructor');
-  var p1 = new Ptolemy();
-  var p2 = new Ptolemy();
-  t.ok(p1._id !== p2._id, 'Different objects have different IDs');
-  t.end();
-});
-
-test('attribute names', function(t) {
-  function Model() {
-    Ptolemy.call(this);
-    this.createAttr('string', String);
-    this.createAttr('number', Number);
-    this.createAttr('date', Date);
-    this.createAttr('object', Object);
-    this.createAttr('function', Function);
-    this.createAttr('ptolemy', Ptolemy);
-    this.createAttr('array', Array);
-  }
-  util.inherits(Model, Ptolemy);
-  var m = new Model();
-  t.equals(m._attrs['string'], 'String', 'Strings works');
-  t.equals(m._attrs['number'], 'Number', 'Number works');
-  t.equals(m._attrs['date'], 'Date', 'Dates works');
-  t.equals(m._attrs['object'], 'Object', 'Objects works');
-  t.equals(m._attrs['function'], 'Function', 'Functions works');
-  t.equals(m._attrs['ptolemy'], 'Ptolemy', 'Ptolemy (a.k.a custom constructors) works');
-  t.equals(m._attrs['array'], 'Array', 'Array works');
-  t.end();
-});
-
-test('attribute type validation', function(t) {
-  function Model() {
-    Ptolemy.call(this);
-    this.createAttr('foo', String);
-    this.createAttr('bar', Number);
-  }
-  util.inherits(Model, Ptolemy);
-  var m = new Model();
-  m.foo = 'works';
-  m.bar = 'oh no! should be a number!';
-  m.save(function(err) {
-    t.equals(err.length, 1, 'There is one error because `foo:String` works, but `bar:String` does not.');
+test('attribute validation', function(t) {
+  var Astronaut = Ptolemy.create('Astronaut');
+  Astronaut.schema = Ptolemy.schema({ name: String, missions: Number });
+  try {
+    var astronaut = Astronaut.create({ name: 'Neil Armstrong', missions: 'two' });
+  } catch(e) {
+    t.ok(e, 'Cannot set the wrong type of schema value');
     t.end();
-  });
+  }
 });
 
-test('hooks', function(t) {
-  var p = new Ptolemy();
-  Ptolemy.beforeSave(function(){});
-  Ptolemy.afterSave(function(){
-    t.ok(true, 'Reaches the after save callback');
-  });
-  t.equals(1, Ptolemy._hooks['beforeSave'].length, 'It registers one before save hooks');
-  t.equals(1, Ptolemy._hooks['afterSave'].length, 'It registers one after save hooks');
-  Ptolemy.beforeSave(function() {
-    t.equals(this.id, p.id, 'It properly stores the context');
-    t.end();
-  });
-  p.save();
-});
+// test('attribute names', function(t) {
+//   function Model() {
+//     Ptolemy.call(this);
+//     this.createAttr('string', String);
+//     this.createAttr('number', Number);
+//     this.createAttr('date', Date);
+//     this.createAttr('object', Object);
+//     this.createAttr('function', Function);
+//     this.createAttr('ptolemy', Ptolemy);
+//     this.createAttr('array', Array);
+//   }
+//   util.inherits(Model, Ptolemy);
+//   var m = new Model();
+//   t.equals(m._attrs['string'], 'String', 'Strings works');
+//   t.equals(m._attrs['number'], 'Number', 'Number works');
+//   t.equals(m._attrs['date'], 'Date', 'Dates works');
+//   t.equals(m._attrs['object'], 'Object', 'Objects works');
+//   t.equals(m._attrs['function'], 'Function', 'Functions works');
+//   t.equals(m._attrs['ptolemy'], 'Ptolemy', 'Ptolemy (a.k.a custom constructors) works');
+//   t.equals(m._attrs['array'], 'Array', 'Array works');
+//   t.end();
+// });
+
+// test('attribute type validation', function(t) {
+//   function Model() {
+//     Ptolemy.call(this);
+//     this.createAttr('foo', String);
+//     this.createAttr('bar', Number);
+//   }
+//   util.inherits(Model, Ptolemy);
+//   var m = new Model();
+//   m.foo = 'works';
+//   m.bar = 'oh no! should be a number!';
+//   m.save(function(err) {
+//     t.equals(err.length, 1, 'There is one error because `foo:String` works, but `bar:String` does not.');
+//     t.end();
+//   });
+// });
+
+// test('hooks', function(t) {
+//   var p = new Ptolemy();
+//   Ptolemy.beforeSave(function(){});
+//   Ptolemy.afterSave(function(){
+//     t.ok(true, 'Reaches the after save callback');
+//   });
+//   t.equals(1, Ptolemy._hooks['beforeSave'].length, 'It registers one before save hooks');
+//   t.equals(1, Ptolemy._hooks['afterSave'].length, 'It registers one after save hooks');
+//   Ptolemy.beforeSave(function() {
+//     t.equals(this.id, p.id, 'It properly stores the context');
+//     t.end();
+//   });
+//   p.save();
+// });
 
